6bebd392fcca0e3bf38271f40bbe0019d9fcb0b3
Warren Lockhart <wlockhart@rippleffect.com>
2017-01-19 12:11:10 +0000

    Create homepage controller and use layout

---
+++ Cloudify/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace Cloudify.Controllers
+{
+    /// <summary>
+    /// The controller for homepage requests.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// GETs the homepage.
+        /// </summary>
+        /// <returns>The homepage view.</returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
